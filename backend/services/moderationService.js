f7ddcd74a5e2592657262da9534e2a2f850c22cf
DNKDevAvi <avishekdas478@gmail.com>
2026-05-02 23:17:17 +0530

    fixing

--- backend/services/moderationService.js
+++ backend/services/moderationService.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const Report = require('../models/Report');
 const aiDetection = require('./aiDetectionService');
 const emailService = require('../utils/emailService');
